9cee68b2513665ab389651e1d58c6751f916e2a3
Minze <gustavogarone@protonmail.com>
2025-05-11 21:52:28 -0300

    tentanto integrar

--- Maio/meio-aleatorio.jl
+++ Maio/meio-aleatorio.jl
@@ -1,22 +1,40 @@
-using Symbolics, LinearSolve, LinearSolve
+using Symbolics, LinearAlgebra, LinearSolve, Integrals
 
 function main(n::Int64)
 
-  function resolve_sistema()
-    @variables μ[1:n]
-    @variables p[1:n]
+  @variables μ[1:n]
+  @variables prob[1:n]
 
+  function resolve_sistema()
     eqs::Vector{Real} = [μ[1]]
     for i in 2:(n-1)
-      append!(eqs, p[i] * μ[i+1] + (1 - p[i]) * μ[i-1] + 1 - μ[i])
+      append!(eqs, prob[i] * μ[i+1] + (1 - prob[i]) * μ[i-1] + 1 - μ[i])
     end
     append!(eqs, μ[n])
     return symbolic_linear_solve(eqs .~ 0, collect(μ))
   end
 
-  resolve_sistema()
-  function integrar()
-
+  function integrar(sisSol, i)
+    expr = simplify(sisSol[i])
+    expr += 0 * (prob[1:n])
+    println(expr)
+    funcao_expressao = build_function(expr, prob)
+    println("1")
+    # avaliavel = eval(funcao_expressao)
+    println("2")
+    # f(prob, p) = avaliavel(prob)
+    f(valores, p) = substitute(funcao_expressao,
+                               Dict(prob[i]=>valores[i] for i in 2:(n-1)))
+    println("3")
+    println("4")
+    dominio = (0.1ones(n-2), 0.9ones(n-2))
+    println("5")
+    integral = IntegralProblem(f, dominio)
+    println("6")
+    return solve(integral, HCubatureJL())
   end
+
+  integrar(resolve_sistema(), 2)
 end
 
+main(5)
