bd2caeed9b153bf7edbc175f058d1378c99e9032
Minze <gustavogarone@protonmail.com>
2025-09-09 22:30:48 -0300

    pullll

--- relatorio-final/yukio/Resolve_Sistemas-Heatmap.jl
+++ relatorio-final/yukio/Resolve_Sistemas-Heatmap.jl
@@ -12,26 +12,25 @@ function main(n, probs)
     println("Comecei")
 
     # Cria uma matriz de elementos μ[i,j], tempo médio começando em i,j
-    @variables μ[1:(n+1), 1:(n+1)] p q r s
-      
+    @variables μ[1:(n + 1), 1:(n + 1)] p q r s
+
     p = big(probs[1])
     q = big(probs[2])
     r = big(probs[3])
     s = big(probs[4])
 
-    if round(p+q+r+s, digits = 2) != 1
+    if round(p + q + r + s, digits = 2) != 1
         error("Probabilidades não somam 1")
     end
 
-    
 
     # Inicializa o vetor de equações
     eqs = []
 
-    # Define as condições de fronteira 
+    # Define as condições de fronteira
     for i in eachindex(μ)
-        if 1 in Tuple(i) || (n+1) in Tuple(i)
-            push!(eqs, μ[i] ~0)
+        if 1 in Tuple(i) || (n + 1) in Tuple(i)
+            push!(eqs, μ[i] ~ 0)
         end
     end
 
@@ -39,31 +38,38 @@ function main(n, probs)
     # EDF  ->  μ[i+1,j]*p + μ[i-1,j]*q + μ[i,j+1]*r + μ[i,j-1]*s + 1 ~ μ[i,j]
     for i in 2:n
         for j in 2:n
-            push!(eqs, μ[i+1,j]*p + μ[i-1,j]*q + μ[i,j+1]*r + μ[i,j-1]*s + 1 ~ μ[i,j])
+            push!(eqs, μ[i + 1, j] * p + μ[i - 1, j] * q + μ[i, j + 1] * r + μ[i, j - 1] * s + 1 ~ μ[i, j])
         end
     end
-    
+
     # Resolver o sistema para μ em termos de p
     sol = symbolic_linear_solve(eqs, μ)
     println("p: $p q: $q r:$r s:$s")
-    return reshape(μ,n+1,n+1), reshape(round.(sol,digits = 2),n+1,n+1), eqs
+    return reshape(μ, n + 1, n + 1), reshape(round.(sol, digits = 2), n + 1, n + 1), eqs
 
 end
 
-n=10
+n = 10
 
 p = 0.4
-q = 0.5-p
+q = 0.5 - p
 r = p
 s = q
 
-probs = (p,q,r,s)
+probs = (p, q, r, s)
 
 (mus, teoricos, eqs) = main(n, probs)
 
-myArray = NamedArray( reverse(teoricos, dims = 1), (0:n, 0:n), ("i", "j"))
+myArray = NamedArray(reverse(teoricos, dims = 1), (0:n, 0:n), ("i", "j"))
 show(stdout, "text/plain", myArray)
 
-j = Int(ceil(n/20))
-heatmap(teoricos,  title="Esperanças dos Tempos", xticks = (1:j:n+1, 0:j:n), yticks = (1:j:n+1, 0:j:n))
-
+j = Int(ceil(n / 20))
+heatmap(
+    teoricos, title = "Esperanças dos Tempos",
+    xticks = (1:j:(n + 1), 0:j:n),
+    yticks = (1:j:(n + 1), 0:j:n),
+    guidefontfamily = "times",
+    tickfontfamily = "times",
+    legendfontfamily = "times",
+)
+savefig("./heatmap.pdf")
