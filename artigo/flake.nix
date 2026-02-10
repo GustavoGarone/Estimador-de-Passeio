eb39c88989dd5b915b30ad7472ebb841e495d42d
Gustavo Garone <gustavogarone@protonmail.com>
2026-02-10 16:11:23 -0300

    feat: adiciona conteúdo do artigo do yukio

---
+++ artigo/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Flake for summer course on spatial statistics";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      my-tex = pkgs.texlive.combine {
+        inherit
+          (pkgs.texlive)
+          scheme-small
+          framed
+          lualatex-math
+          collection-latexextra
+          algorithmicx
+          algorithms
+          ;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.pkg-config];
+
+        buildInputs = with pkgs; [
+          quarto
+          my-tex
+        ];
+      };
+    });
+}
